4e6d31a462b6f0cfbb4e9d1610319467a4d5d37b
Ubuntu <ubuntu@ip-172-31-89-58.ec2.internal>
2025-07-17 15:48:09 +0000

    version1

---
+++ aws_resource_tracker4.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+
+##########################
+#Author : sailakshmi
+#
+#date : 6th july
+#
+#version : v1
+#
+###########################
+#
+#
+#set -x
+#
+# aws s3
+# aws ec2
+# aws lambda
+# aws IAM Users
+
+touch "list the s3 buckets"
+aws s3 ls
+
+
+touch "list the ec2 instance0s"
+aws ec2 describe-instances
+
+touch "list lambda functions"
+aws lambda list-functions
+
+touch "list IAM Users"
+ aws iam list-users
+
+~
+~
+~
+~
+~
+~
+~
+
